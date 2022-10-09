b817f59cb01246f29de83c039174a9c67b831e62
Ori <orizerahmc@gmail.com>
2022-10-09 10:13:55 +0300

    Added global GetFileLink

---
+++ global/filelink.go
@@ -0,0 +1,31 @@
+package global
+
+import (
+	"github.com/argonsecurity/go-environments/enums"
+	"github.com/argonsecurity/go-environments/environments/azure"
+	"github.com/argonsecurity/go-environments/environments/bitbucket"
+	"github.com/argonsecurity/go-environments/environments/github"
+	"github.com/argonsecurity/go-environments/environments/gitlab"
+)
+
+type GetFileLinkFunc func(string, string, string, int, int) string
+
+func GetFileLink(source enums.Source, repositoryURL string, filename string, ref string, startLine int, endLine int) string {
+	var f GetFileLinkFunc
+	switch source {
+	case enums.Github, enums.GithubServer:
+		f = github.GetFileLink
+	case enums.Gitlab, enums.GitlabServer:
+		f = gitlab.GetFileLink
+	case enums.Azure, enums.AzureServer:
+		f = azure.GetFileLink
+	case enums.Bitbucket, enums.BitbucketServer:
+		f = bitbucket.GetFileLink
+	}
+
+	if f != nil {
+		return f(repositoryURL, filename, ref, startLine, endLine)
+	}
+
+	return ""
+}
